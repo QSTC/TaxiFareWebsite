04746f2a6012adc9a543eaf2f38e6c294630a5e5
Quentin Sustrac <qsustrac@gmail.com>
2021-08-20 15:32:07 +0200

    new funct : side panel + static map

--- app.py
+++ app.py
@@ -1,24 +1,47 @@
 import streamlit as st
 import datetime
 import requests
+import folium
+from streamlit_folium import folium_static
 
-'''
-#       🚕 TaxiFareModel front 🚕
-'''
+st.markdown("<h1 style='text-align: center; color: red;'>🚕 TAXI PAS CHER 🚕</h1>",
+            unsafe_allow_html=True)
 
+col1, col2, col3 = st.columns([1, 6, 1])
 
-st.markdown('Balance les infos sur ta course en tacos !!!')
-date = st.date_input('Indicate pick up date')
-time = st.time_input('Indicate pick up time')
+### Side menu for data input
+
+button_status=st.sidebar.button('🎰 calcul hyper compliqué 🎰')
+
+st.sidebar.markdown('Balance les infos sur ta course en tacos 🌮')
+date = st.sidebar.date_input('Indicate pick up date')
+time = st.sidebar.time_input('Indicate pick up time')
 
 pickupdatetime = datetime.datetime.combine(date,time)
-st.markdown(pickupdatetime)
+st.sidebar.markdown(pickupdatetime)
 
-pickuplong = st.number_input('Indicate pickup longitude')
-pickuplat = st.number_input('Indicate pickup latitude')
-dropofflong = st.number_input('Indicate dropoff longitude')
-dropofflat = st.number_input('Indicate dropoff latitude')
-passcount = st.slider('Passenger Count', min_value=1, max_value=8, value=5, step=1)
+pickuplong = st.sidebar.number_input('Indicate pickup longitude')
+pickuplat = st.sidebar.number_input('Indicate pickup latitude')
+dropofflong = st.sidebar.number_input('Indicate dropoff longitude')
+dropofflat = st.sidebar.number_input('Indicate dropoff latitude')
+passcount = st.sidebar.slider('Passenger Count',
+                              min_value=1,
+                              max_value=8,
+                              value=5,
+                              step=1)
+
+# center on Liberty Bell
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+
+# add marker for Liberty Bell
+tooltip = "Liberty Bell"
+folium.Marker(
+        [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
+    ).add_to(m)
+
+# call to render Folium map in Streamlit
+folium_static(m)
+
 
 ## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
 
@@ -62,14 +85,25 @@ gif1 = 'https://media3.giphy.com/media/l41Ys1fQky5raqvMQ/giphy.gif?cid=790b76111
 gif2 = 'https://media4.giphy.com/media/3ohze3kG5qO9DcTUbe/giphy.gif?cid=ecf05e4736bb926c6883196942a0e7e83f978d433646fe06&rid=giphy.gif&ct=g'
 #<iframe src="https://giphy.com/embed/3ohze3kG5qO9DcTUbe" width="480" height="333" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/foxhomeent-3ohze3kG5qO9DcTUbe">via GIPHY</a></p>
 
-if pred>10:
-    st.image(gif2)
-    st.write(f'💰💸💰💸💰💸💰 CA VA TE COUTER {pred}$$ 🤑💵🤑💵🤑💵🤑 !!!!')
+with col2:
+    if button_status:
+        if pred > 10:
+            st.image(gif2)
+            st.write(f'😱🥶😱🥶😱 {pred}$$ C TROP REUCH !!!')
 
-if pred < 10:
-    st.balloons()
-    st.image(gif1)
-    st.write(f'💰💸💰💸💰💸💰 CA VA TE COUTER {pred}$$ 🤑💵🤑💵🤑💵🤑 !!!!')
+
+        if pred < 10:
+            st.balloons()
+            st.image(gif1)
+            st.write(f'💰💸💰💸💰💸💰 CA VA TE COUTER {pred}$$ 🤑💵🤑💵🤑💵🤑 !!!!')
+
+
+
+
+
+
+
+
 
 
 #<iframe src="https://giphy.com/embed/l41Ys1fQky5raqvMQ" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/angrybirds-angry-birds-leonard-movie-l41Ys1fQky5raqvMQ">via GIPHY</a></p>
